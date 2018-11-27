996f7d89adda21a3a0eb051a3fddfc06006b85a9
claytonknittel <c.j.knittel@wustl.edu>
2018-11-27 15:57:21 -0600

    abstraction for decision trees

--- clayton/decision_tree.py
+++ clayton/decision_tree.py
@@ -2,21 +2,8 @@ import numpy as np
 import itertools
 import operator
 import csv
-
-
-def most_common_label(vector, attr):
-    vector.sort(key=lambda x: int(x[attr]))
-    groups = itertools.groupby(vector, key=operator.itemgetter(attr))
-    maxels = 0
-    maxlabel = -1
-    for g in groups:
-        length = 0
-        for _ in g[1]:
-            length += 1
-        if length > maxels:
-            maxels = length
-            maxlabel = g[0]
-    return maxlabel, maxels / len(vector)
+import random
+from clayton.stats import discreet_distribution
 
 
 def intval(str):
@@ -32,17 +19,17 @@ def intval(str):
 def all_labels_with_probs(vector, attr):
     vector.sort(key=lambda x: intval(x[attr]))
     groups = itertools.groupby(vector, key=operator.itemgetter(attr))
-    group = []
+    group = discreet_distribution()
     for g in groups:
         l = []
         for item in g[1]:
             l.append(item[attr])
-        group.append((l[0], len(l) / len(vector)))
-    group.sort(key=lambda x: -x[1])
+        group.add(l[0], occurences=len(l))
     return group
 
 
 def split(vectors, category):
+    # split the vectors by the category at index category
     vectors.sort(key=lambda x: intval(x[category]))
     groups = itertools.groupby(vectors, key=operator.itemgetter(category))
     group = {}
@@ -52,14 +39,6 @@ def split(vectors, category):
             l.append(item)
         group[g[0]] = l
     return group
-    # split the vectors by the category at index category
-    # dic = {}
-    # for vector in vectors:
-    #     if not vector[category] in dic:
-    #         dic[vector[category]] = [vector,]
-    #     else:
-    #         dic[vector[category]].append(vector)
-    # return dic
 
 
 def entropy(vectors, cluster):
@@ -101,12 +80,21 @@ def gain_ratio(vectors, split_attribute, cluster):
     return ig / si
 
 
+def bootstrap_sample(vectors, size):
+    ret = []
+    for x in range(0, size):
+        ret.append(vectors[random.randint(0, len(vectors))])
+    return ret
+
+
 class decision_tree:
 
-    def __init__(self, vectors, cluster_location=None, attrlist=None, min_leaf_size=1):
+    def __init__(self, vectors, cluster_location=None, attrlist=None, min_leaf_size=1, m=-1):
         # vectors is a list of data points of categorical (integral)
         # data. The attribute that is assumed to be the cluster identifier
         # is the first unless otherwise specified
+        # n is the number of features to be considered per split
+        self.m = m
         self.min_leaf_size = min_leaf_size
         if cluster_location is None:
             cluster_location = 0
@@ -119,36 +107,48 @@ class decision_tree:
         n = decision_tree._node()
         n.parent = parent
         if len(subset) == 0:
-            n.cluster_labels.append((-1, 0))
+            n.cluster_labels.add(-1, 1)
             return n
         if len(subset) <= self.min_leaf_size or len(attrlist) == 0:
             n.cluster_labels = all_labels_with_probs(subset, self.cluster_loc)
             return n
         if entropy(subset, self.cluster_loc) == 0:
-            n.cluster_labels.append((subset[0][self.cluster_loc], 1))
+            n.cluster_labels.add(subset[0][self.cluster_loc], occurences=len(subset))
             return n
 
         max_gainratio = -1
         best_attrloc = -1
-        for i in range(0, len(attrlist)):
-            gainratio = gain_ratio(subset, attrlist[i], self.cluster_loc)
+
+        if self.m == -1:
+            at = attrlist
+        else:
+            if self.m >= len(attrlist):
+                at = attrlist
+            else:
+                at = random.sample(attrlist, self.m)
+
+        for i in range(0, len(at)):
+            gainratio = gain_ratio(subset, at[i], self.cluster_loc)
             if gainratio > max_gainratio:
                 max_gainratio = gainratio
                 best_attrloc = i
 
         n.cluster_labels = all_labels_with_probs(subset, self.cluster_loc)
-        splits = split(subset, attrlist[best_attrloc])
-        n.val = attrlist[best_attrloc]
-        newattrs = attrlist[:best_attrloc] + attrlist[best_attrloc + 1:]
+        splits = split(subset, at[best_attrloc])
+        n.val = at[best_attrloc]
+        newattrs = attrlist.copy()
+        newattrs.remove(at[best_attrloc])
         for sub in splits:
             n.children.append((self._create_tree(n, splits[sub], newattrs), sub))
+            print(sub)
         return n
 
     def classify(self, datapt):
         n = self._get_node(datapt, self._root)
-        return n.val
+        return n.cluster_labels
 
     def _get_node(self, datapt, node):
+        print(str(node.val) + ' ' + str(node.cluster_labels))
         if node.val is None:
             return node
         return self._get_node(datapt, node[datapt[self.cluster_loc]])
@@ -173,7 +173,7 @@ class decision_tree:
             # used if this is a leaf node, to determine which cluster
             # the leaf belongs to (gives sorted list of clusters by
             # their probabilities/confidences)
-            self.cluster_labels = []
+            self.cluster_labels = discreet_distribution()
             self.children = []
             self.parent = None
 
@@ -188,6 +188,32 @@ class decision_tree:
                 return str(self.val) + ' ' + str(self.cluster_labels)
             else:
                 return 'l ' + str(self.cluster_labels)
+
+
+class random_forest:
+
+    def __init__(self, vectors, cluster_location=None, attrlist=None, min_leaf_size=1, T=1, n=1, m=-1):
+        # T is total number of trees
+        # n is the size of the bagged clusters (number of data pts per bag)
+        # m is number of attributes to choose from attrlist for each split
+        self.n = n
+        self.m = m
+        self.trees = []
+        for x in range(0, T):
+            sample = bootstrap_sample(vectors, n)
+            self.trees.append(decision_tree(sample, cluster_location=cluster_location, attrlist=attrlist,
+                                            min_leaf_size=min_leaf_size, m=m))
+
+    def classify(self, datapt):
+        ret = {}
+        for tree in self.trees:
+            res = tree.classify(datapt)
+            for vp in res:
+                if vp[0] in ret:
+                    ret[vp[0]] += vp[1]
+                else:
+                    ret[vp[0]] = vp[1]
+        return ret
 
 
 if __name__ == '__main__':
@@ -205,14 +231,17 @@ if __name__ == '__main__':
 
         v = []
         count = 0
+        tester = None
         for f in c:
+            if tester is None:
+                tester = f
             if f['severity'] == '-1':
                 continue
             v.append({})
             for k, val in f.items():
                 v[-1][k] = val
             count += 1
-            if count == -1:
+            if count == 100:
                 break
 
         l = list(c.fieldnames)
@@ -226,7 +255,8 @@ if __name__ == '__main__':
         l.remove('month')
         l.remove('year')
         l.remove('collision type')
-        tree = decision_tree(v, cluster_location='severity', attrlist=l, min_leaf_size=1000)
+        tree = decision_tree(v, cluster_location='severity', attrlist=l, min_leaf_size=1)
         print(tree)
-
+        print(tester)
+        print('clas', tree.classify(tester))
 
---
+++ clayton/stats.py
@@ -0,0 +1,43 @@
+
+class _pt:
+
+    def __init__(self, label, probability):
+        self.label = label
+        self.probability = probability
+
+
+class discreet_distribution:
+
+    def __init__(self):
+        self._dict = []
+        self._size = 0
+
+    def add(self, item, occurences=1):
+        self._size += occurences
+        for i in self._dict:
+            if i[0] == item:
+                i[1] += occurences
+                return
+        self._dict.append((item, occurences))
+        self._dict.sort(key=lambda i: -i[1])
+
+    def probability(self, item):
+        for i in self._dict:
+            if i[0] == item:
+                return i[1] / len(self)
+
+    def most_likely(self):
+        return _pt(self._dict[0][0], self._dict[0][1] / len(self))
+
+    def pts(self):
+        for i in self._dict:
+            yield _pt(i[0], i[1] / len(self))
+
+    def __len__(self):
+        return self._size
+
+    def __repr__(self):
+        ret = ''
+        for pt in self.pts():
+            ret += '({}:{:.2f}) '.format(pt.label, pt.probability)
+        return ret
